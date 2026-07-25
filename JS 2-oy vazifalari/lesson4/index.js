15fdb4a6b529378ce16b6ebec882469b210cc968
asqaraliyevo38-cmyk <asqaraliyevo38@gmail.com>
2026-07-25 17:29:47 +0500

    musavvir githubni o'rgandi.

--- JS 2-oy vazifalari/lesson4/index.js
+++ JS 2-oy vazifalari/lesson4/index.js
@@ -1,1 +1,57 @@
-console.log("Alik");
+// // startwith
+// let meva = "olma";
+// console.log(meva.startsWith("ol"));
+
+
+// // endsWith
+
+// let daraxt = "daraxt";
+// console.log(daraxt.endsWith("xt"));
+
+
+// // search
+
+// let aloqa = "telefon";
+// console.log(aloqa.search("fon"));
+
+
+// // indexOf
+
+// let transport = "mashina";
+// console.log(transport.indexOf("h"));
+
+// // lastIndexOf
+
+// let meva2 = "banan";
+// console.log(meva2.lastIndexOf("a"));
+
+
+// // padStart
+
+// let son1 = "9";
+// console.log(son1.padStart(3, "0"));
+
+
+// // padEnd
+
+// let raqam1 = "8";
+// console.log(raqam1.padEnd(3, "0"));
+
+
+// // repeat
+
+// let soz1 = "ha";
+// console.log(soz1.repeat(2));
+
+
+// // replace
+
+// let meva3 = "anor";
+// console.log(meva3.replace("a", "o"));
+
+
+// // replaceAll
+
+// let matn1 = "lala";
+// console.log(matn1.replaceAll("a", "o"));
+
--- js 2-etap/3-dars/app.js
+++ js 2-etap/3-dars/app.js
@@ -11,25 +11,25 @@ console.log(obj?.child?.ism);
 
 // stringlar 
 
-//1.'' 
+//dd.'' 
 //2.""
 //3.``
 //4.string
 //5.tostring ❌ new string()-- condtractor ❌❌
 
-// let yil = 2026;
-// let matn = `bu yil hozir ${yil}-yil`;
-// console.log(matn);
+let yil = 2026;
+let matn = `bu yil hozir ${yil}-yil`;
+console.log(matn);
  
 
-// let str = "oxford";
-// console.log(str[0]);
-// console.log(str[1]);
-// console.log(str[5]);
+let strr = "oxford";
+console.log(strr[0]);
+console.log(strr[dd]);
+console.log(strr[5]);
 
 
 // loop
-//1 for
+//dd for
 for (let i = 0 ; i< str .length; i++){
     console.log(str [i]);
 }
@@ -42,15 +42,15 @@ for (let value of str){
     
 }
 
-// let a = "A";
-// let b ="a";
-// console.log("A">"a");
-// console.log("A"<"a");
+let a = "A";
+let b ="a";
+console.log("A">"a");
+console.log("A"<"a");
 
 // let a = "Algoritm";
 // let b ="Agoritm";
 
-// console.log(a>b);
+console.log(a>b);
 
 // {]]{}}}}}}}}}}}}}}}}}}}}]]]]][][]]
 // {]]{}}}}}}}}}}}}}}}}}}}}]]]]][][]]
@@ -59,30 +59,30 @@ for (let value of str){
 
 // methods
 
-//1 length 
+//dd length 
 let str = "Agoritm";
 console.log(str .length);
 
 // 2 charAt 
 
-// console.log(str [0]);
-// console.log(str.charAt(0));
-// console.log(atr .at(0));
+console.log(str [0]);
+console.log(str.charAt(0));
+console.log(atr .at(0));
 
-// console.log(str [-1]);❌
-// console.log(str.charAt(-1));❌
-// console.log(atr .at(-1));👍👍
-// console.log(atr .at(-8));👍👍
+console.log(str [-dd]);
+console.log(str.charAt(-1));
+console.log(atr .at(-1));
+console.log(atr .at(-8));
 
 //charcodeat 
 // console.log(str .charCodeAt(1));
 
 
-// let mant1 = "salom";
-// let mant2 = ",";
-// let mant3 = "";
-// let mant4 = "yaxshimisiz";
-// // let matn = matn1+matn2+matn3+mant4;
-// // let matn= matn1.concat(mant2,mant3,matn4)
-// // console.log(matn);
+let mant1 = "salom";
+let mant2 = ",";
+let mant3 = "";
+let mant4 = "yaxshimisiz";
+// let matn = matn1+matn2+matn3+mant4;
+// let matn= matn1.concat(mant2,mant3,matn4)
+// console.log(matn);
 
