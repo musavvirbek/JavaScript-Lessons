72dfc17fa055f032945e4eb5a0b0f7745d6dc7e2
musavvirbek <mhamzaxonov@gmail.com>
2026-07-28 18:33:01 +0500

    masala qo'shildi

--- js 2-etap/5-dars/app.js
+++ js 2-etap/5-dars/app.js
@@ -1,11 +1,25 @@
-let str = "Men Oxforda IT o'qiyman!";
-function reverseStr(str){
-    let newStr ="";
-    for (let i = str .length - 1 ;i  >= 0 ; i --){
-      newStr += str.at (i);
+// let str = "Men Oxforda IT o'qiyman!";
+// function reverseStr(str){
+//     let newStr ="";
+//     for (let i = str .length - 1 ;i  >= 0 ; i --){
+//       newStr += str.at (i);
   
-    }
-    return newStr ;  
-}
-console.log(reverseStr(str));
+//     }
+//     return newStr ;  
+// }
+// console.log(reverseStr(str));
 
+
+// ====================================
+
+// 2 
+// let str ="Oxford";
+// function getSum (str){
+//     let sum = 0 ;
+//     for (let value of str ){
+//         sum += value.charCodeAt();
+//     }
+//     return sum 
+// }
+    
+// console.log(getSum(str));
