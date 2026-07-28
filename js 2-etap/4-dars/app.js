27971ab56d42ee6d2444428308d0788685f3767d
asqaraliyevo38-cmyk <asqaraliyevo38@gmail.com>
2026-07-28 15:29:59 +0500

    yangi vazifa qoshildi

---
+++ js 2-etap/4-dars/app.js
@@ -0,0 +1,44 @@
+// 1  slice - matndan nusxa  olish uchun 
+// let str ="oxford salom salom ";
+// // let copyone =str .slice(0,6);
+// // slice(startIndex,endIndex+ 1)
+// console.log(copyone);
+
+ 
+//2  console.log(str.substring(-6));❌
+// slice bilan birxil ihslaydi faqat manfiiy index qabu; qilmaydi 
+
+// 3 split -srtingni arayyga aylantirab beradi 
+// let str= "oxford ";
+// ['o','x','f','o','r','d']
+// let arr =str.split("");
+// console.log(arr);
+
+// let str ="men oxforda js organmoqdaman ";
+// let arr =str.split( " ");
+// console.log(arr);
+
+// 4 trim - 
+// let str = "      oxford va algoritm         ";
+// console.log(str);
+// console.log(str.length);
+// console.log(str.trim());  
+// console.log(str.trim().length);  
+
+// let num =3029433;
+// console.log(num.toString());
+
+// let str ="salom";
+// console.log(str .toUpperCase());
+// console.log(str .toLocaleLowerCase());
+
+// 6 localecompeir
+
+// let a ="b";
+// let b ="a ";
+
+// console.log(a.localeCompare(b));
+
+// 7 eval str ichidagi matematik ammalarni to'g'ri ishlatib beradi 1.`~ 
+// let sum ="19 -9 +(2*5)/10";
+// console.log(eval(sum));
