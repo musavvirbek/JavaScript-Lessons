4a10b159a0cc32ffd00889d7028cf18ccfe64f85
musavvirbek <mhamzaxonov@gmail.com>
2026-08-08 16:50:37 +0500

    yangi vazifa qo'shdi musavvir

---
+++ JS 2-oy vazifalari/8-vazifa/app.js
@@ -0,0 +1,231 @@
+
+// const arr1 = [1, 2, 3];
+
+// arr1.push(4);
+
+// console.log(arr1);
+
+
+
+// const arr2 = [1, 2, 3, 4, 5, 6];
+// const newArr2 = [];
+
+// arr2.forEach(item => {
+//     if (item % 2 === 0) {
+//         newArr2.push(item);
+//     }
+// });
+
+// console.log(newArr2);
+
+
+
+// const arr3 = [10, 20, 30];
+
+// arr3.pop();
+
+// console.log(arr3);
+
+
+
+// const arr4 = [1, 2, 3, 4, 5];
+
+// arr4.pop();
+// arr4.pop();
+
+// console.log(arr4);
+
+
+
+// const arr5 = [2, 3];
+
+// arr5.unshift(1);
+
+// console.log(arr5);
+
+
+
+// const arr6 = [1, 2, 3];
+// const newArr6 = [];
+
+// arr6.forEach(item => {
+//     newArr6.unshift(item);
+// });
+
+// console.log(newArr6);
+
+
+
+// const arr7 = [1, 2, 3];
+
+// arr7.shift();
+
+// console.log(arr7);
+
+
+
+// const arr8 = [1, 2, 3, 4];
+
+// arr8.shift();
+// arr8.shift();
+
+// console.log(arr8);
+
+
+
+// const arr9 = [10, 20, 30, 40];
+
+// delete arr9[2];
+
+// console.log(arr9);
+
+
+
+// const arr10 = [10, 30, 20, 30];
+
+// delete arr10[arr10.indexOf(30)];
+
+// console.log(arr10);
+
+
+
+// const arr11 = [1, 2, 3, 4];
+
+// console.log(arr11.length);
+
+
+
+// const arr12 = [1, 2, 3, 4, 5];
+
+// arr12.length = 3;
+
+// console.log(arr12);
+
+
+
+// const arr13 = [5, 10, 15];
+
+// console.log(arr13.at(-1));
+
+
+
+// const arr14 = [1, 2, 3, 4];
+
+// console.log(arr14.at(-2));
+
+
+
+// const arr15 = [10, 20, 30];
+
+// console.log(arr15.indexOf(20));
+
+
+
+// const arr16 = [1, 2, 3];
+// const value16 = 4;
+
+// console.log(arr16.indexOf(value16));
+
+
+
+// const arr17 = [1, 2, 3, 2];
+
+// console.log(arr17.lastIndexOf(2));
+
+
+
+// const arr18 = [1, 2, 3, 2, 4];
+
+// console.log(arr18.lastIndexOf(2));
+
+
+
+// const arr19 = [1, 2, 3, 4];
+
+// console.log(arr19.includes(5));
+
+
+
+// const arr20 = [1, 3, 5, 6];
+
+// console.log(arr20.includes(6));
+
+
+
+// const str21 = "Hello world";
+
+// console.log(str21.split(" "));
+
+
+
+// const str22 = "Hi A";
+
+// console.log(str22.split(""));
+
+
+
+// const arr23 = ["a", "b", "c"];
+
+// console.log(arr23.join("-"));
+
+
+
+// const arr24 = ["Men", "dasturchiman"];
+
+// console.log(arr24.join(" ") + ".");
+
+
+
+// const arr25 = [1, 2, 3];
+
+// console.log(arr25.reverse());
+
+
+
+// const str26 = "hello";
+
+// console.log(str26.split("").reverse().join(""));
+
+
+
+// const arr27 = [1, 2];
+// const arr28 = [3, 4];
+
+// console.log(arr27.concat(arr28));
+
+
+
+// const arr29 = [1];
+// const arr30 = [2];
+// const arr31 = [3];
+
+// console.log(arr29.concat(arr30, arr31));
+
+
+
+// const arr32 = [0, 1, 2, 3, 4];
+
+// console.log(arr32.slice(1, 3));
+
+
+
+// const arr33 = [1, 2, 3, 4, 5];
+
+// console.log(arr33.slice(-3));
+
+
+
+// const arr34 = [1, 2, 3, 4];
+
+// arr34.splice(2, 1);
+
+// console.log(arr34);
+
+
+
+// const arr35 = [1, 2, 3, 4];
+
+// arr35.splice(1, 2, 9, 9);
+
+// console.log(arr35);
+
---
+++ js 2-etap/6-vazifa/app.js
@@ -0,0 +1,9 @@
+// let = one ["olma,'anor,banan"]
+
+// two.pop()
+// console.log(two);
+
+// one.push("olma")
+// console.log(one);
+
+
