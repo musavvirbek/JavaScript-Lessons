6a74324141ce63c3e50f2e9e6baccaa0ea0e198e
musavvirbek <mhamzaxonov@gmail.com>
2026-08-20 15:06:55 +0500

    yan yangi vazifa qo'shildi

---
+++ js 2-etap/8-dars/app.js
@@ -0,0 +1,83 @@
+// //1 
+// function expressionMatter(a, b, c) {
+//   return Math.max(
+//     a + b + c,
+//     a * b * c,
+//     a + b * c,
+//     (a + b) * c,
+//     a * (b + c),
+//     a * b + c,
+//     a + b * c
+//   );
+// }
+
+// //2 
+
+
+// To square(root) or not to square(root)
+// JavaScript:
+// function squareOrSquareRoot(array) {
+//   return array.map(num => {
+//     const sqrt = Math.sqrt(num);
+
+//     return Number.isInteger(sqrt) ? sqrt : num ** 2;
+//   });
+// }
+// 6 minutes agoRefactor
+
+// Find Nearest square number
+// JavaScript:
+// function nearestSq(n) {
+//   return Math.round(Math.sqrt(n)) ** 2;
+// }
+// 8 minutes agoRefactorDiscuss
+
+
+// Get Planet Name By ID
+// JavaScript:
+// function getPlanetName(id) {
+//   var name;
+
+//   switch (id) {
+//     case 1:
+//       name = 'Mercury';
+//       break;
+//     case 2:
+//       name = 'Venus';
+//       break;
+//     case 3:
+//       name = 'Earth';
+//       break;
+//     case 4:
+//       name = 'Mars';
+//       break;
+//     case 5:
+//       name = 'Jupiter';
+//       break;
+//     case 6:
+//       name = 'Saturn';
+//       break;
+//     case 7:
+//       name = 'Uranus';
+//       break;
+//     case 8:
+//       name = 'Neptune';
+//       break;
+//   }
+
+//   return name;
+// }
+
+// //3 
+// function nearestSq(n) {
+//   return Math.round(Math.sqrt(n)) ** 2;
+// }
+
+// //4 
+// function squareOrSquareRoot(array) {
+//   return array.map(num => {
+//     const sqrt = Math.sqrt(num);
+
+//     return Number.isInteger(sqrt) ? sqrt : num ** 2;
+//   });
+// }
