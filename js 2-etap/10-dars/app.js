350fd2148d782ff157426d65038c85be15e93310
musavvirbek <mhamzaxonov@gmail.com>
2026-08-27 17:19:21 +0500

    yangi malumotlar qo'shildi

---
+++ js 2-etap/10-dars/app.js
@@ -0,0 +1,30 @@
+
+// 1	let  ------	 Qiymati keyin o‘zgarishi mumkin	let age = 20
+// 2	const  ----- Qiymati qayta o‘zgartirilmaydi	const name = "Ali"
+// 3	var    -----  Eski usuldagi o‘zgaruvchi	var x = 10
+// 4	String ----- Matn saqlaydi	let name = "Ali"
+// 5	Number ----- Son saqlaydi	let age = 20
+// 6	Boolean ---- true yoki false	let active = true
+// 7	null -----	Ataylab bo‘sh qiymat	let x = null
+// 8	undefined -------	Qiymat berilmagan	let x
+// 9	if --------	Shartni tekshiradi	if (age >= 18)
+// 10	else   -------	 Shart noto‘g‘ri bo‘lsa ishlaydi	else {}
+// 11	switch --------	Bir nechta variantni tekshiradi	switch(day)
+// 12	for  ----------	Takrorlash uchun	for(let i=0;i<5;i++)78   
+// 13	while --------	Shart rost bo‘lguncha ishlaydi	while(x < 5)
+// 14	Function ------	Qayta ishlatiladigan kod	function add(){}
+// 15	Arrow ------- function	Qisqa function	const add = () => {}
+// 16	return --------	Funksiyadan qiymat qaytaradi	return a + b
+// 17	Array --------	Bir nechta qiymat saqlaydi	[1, 2, 3]
+// 18	push() -----	Array oxiriga qo‘shadi	arr.push(4)
+// 19	pop()	 ------ Oxirgi elementni olib tashlaydi	arr.pop()
+// 20	shift() ------	Birinchi elementni olib tashlaydi	arr.shift()
+// 21	unshift() ------	Boshiga element qo‘shadi	arr.unshift(0)
+// 22	map() --------	Har bir elementni o‘zgartirib yangi array beradi	arr.map(x => x * 2)
+// 23	filter() --------	Shartga mos elementlarni oladi	arr.filter(x => x > 5)
+// 24	find() ---------	Birinchi mos elementni topadi	arr.find(x => x > 5)
+// 25	forEach() ---------	Har bir element ustida amal bajaradi	arr.forEach(x => console.log(x))
+// 26	reduce() --------	Arrayni bitta qiymatga yig‘adi	arr.reduce((a,b) => a+b, 0)
+// 27	Object    ---------	key:value ko‘rinishida ma’lumot saqlaydi	{name:"Ali", age:20}
+// 28	Destructuring     ----------	Qiymatlarni ajratib oladi	const {name} = user
+// 29	Spread ---------       ...	Array/objectni yoyadi	[...arr]
