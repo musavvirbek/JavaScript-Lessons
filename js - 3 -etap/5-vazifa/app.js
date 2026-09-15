2315d0d7ea8a16af288ec605df7192076f2888bc
musavvirbek <mhamzaxonov@gmail.com>
2026-09-15 16:06:08 +0500

    yangi qoidalarva fazifalar qo'shiladi

--- js - 3 -etap/5-vazifa/app.js
+++ js - 3 -etap/5-vazifa/app.js
@@ -1,26 +1,27 @@
 
-// class Person{
-//     constructor(ism,familya,yosh,manzil,viloyat,tuman,mahala,maktab,oquvmarkaz,Otasi,Onasi,nimagaqiziqadi,moshna,telnomer){
-//         this.a = ism;
-//         this.b = familya;
-//         this.c = yosh;
-//         this.d = manzil;
-//         this.f = viloyat;
-//         this.e = tuman;
-//         this.g = mahala;
-//         this.j = maktab;
-//         this.v = oquvmarkaz;
-//         this.s = Otasi;
-//         this.w = Onasi;
-//         this.q = nimagaqiziqadi;
-//         this.l = moshna;
-//         this.k = telnomer;
-//     }
-// }
+class Person{
+    constructor(ism,familya,yosh,manzil,viloyat,tuman,mahala,maktab,oquvmarkaz,Otasi,Onasi,nimagaqiziqadi,moshna,telnomer,yil){
+        this.a = ism;
+        this.b = familya;
+        this.c = yosh;
+        this.d = manzil;
+        this.f = viloyat;
+        this.e = tuman;
+        this.g = mahala;
+        this.j = maktab;
+        this.v = oquvmarkaz;
+        this.s = Otasi;
+        this.w = Onasi;
+        this.q = nimagaqiziqadi;
+        this.l = moshna;
+        this.k = telnomer;
+        this.p = yil;
+    }
+}
 
-// let obj1 = new Person("Musavvirxon","Hamzaxonov", 16,"o'zbekiston","namangan","kosonsoy","iftixor",19,"ofxord","Xojixon","Hakimaxon","IT","VAZ07","998 93 668 69 11")
-// for (let key in obj1){
-//     console.log(obj1 [key]);
+let obj1 = new Person("Musavvirxon","Hamzaxonov", 16,"o'zbekiston","namangan","kosonsoy","iftixor",19,"ofxord","Xojixon","Hakimaxon","IT","VAZ07","998 93 668 69 11","2011")
+for (let key in obj1){
+    console.log(obj1 [key]);
 
-// }
+}
 
---
+++ js - 3 -etap/6-dasr/app.js
@@ -0,0 +1,73 @@
+// let ism = "Eshamt";
+
+// let name = "hiii";
+
+// class Person {
+//     [name]() {
+//         return ism;
+//     }
+// }
+// let obj = new Person();
+// // console.log(obj.[name]());
+// console.log(obj.hiii());
+
+
+// let brend1 = "Algoritm";
+// let brend2 = "Oxford";
+
+// class Person {
+//     status = "Web Developer";
+//     constructor(yil) {
+//         this.yil = yil;
+//     }
+//     getBrend() {
+//         return brend1;
+//     }
+// }
+
+// class Child extends Person {
+//     status = "Frontend Dasturchi";
+//     constructor() {
+//         super(2026);
+//         // this.yil= 2050;
+//     }
+//     getBrend() {
+//         return brend2
+//     }
+// }
+// let obj = new Child();
+// console.log(obj.getBrend());
+// console.log(obj.yil);
+// console.log(obj.status);
+
+
+
+// class Animals{
+//     static count = 0;
+//     constructor(ism){
+//         this.ism = ism;
+//         Animals.count++;
+//     }
+
+//     static sum(a,b){
+//         return a * b;
+//     }
+// }
+
+// let cat = new Animals("Masha");
+// let dog = new Animals("simba");
+// let sher = new Animals("sher");
+
+// console.log(Animals.count);
+// console.log(Animals.sum(7,4));
+// console.log(Animals.sum(4,8));
+// console.log(Animals.sum(9,6));
+
+// try {
+//     console.log(ism);
+//     console.log(true.toUpperCase());
+//     console.log("salom".toUpperCase());
+
+//     let obj = {};
+//     console.log(obj?.child?.ism);
+// } catch (error ){}name, ":", error.message);
